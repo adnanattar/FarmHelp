54b272487d2d7b855eb55042727f3beab5e1aaa5
Priti <huparep2020@gmail.com>
2022-05-28 23:24:53 +0530

    structure changes in frontend

--- app/src/main/java/com/ynot/farmhelp/MyFarm.java
+++ app/src/main/java/com/ynot/farmhelp/MyFarm.java
@@ -4,8 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
+
 public class MyFarm extends AppCompatActivity {
 
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+    StorageReference storageReference;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
