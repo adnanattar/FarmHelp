0922de2b0eeb0c1d086349dc348b448b9f28e53a
Priti <huparep2020@gmail.com>
2022-06-11 13:07:56 +0530

    Preeti minor update

--- app/src/main/java/com/ynot/farmhelp/MyFarm.java
+++ app/src/main/java/com/ynot/farmhelp/MyFarm.java
@@ -28,11 +28,11 @@ public class MyFarm extends AppCompatActivity {
         EditText email= findViewById(R.id.email);
         EditText phone = findViewById(R.id.phone);
 
-        Spinner select_crop= findViewById(R.id.select_crop) ;
-        Spinner select_city= findViewById(R.id.select_city) ;
+        Spinner select_crop= findViewById(R.id.select_crop);
+        Spinner select_city= findViewById(R.id.select_city);
         Spinner select_market= findViewById(R.id.select_market);
         Spinner  select_soil = findViewById(R.id.select_soil);
-
+       Spinner select_area=findViewById(R.id.select_area);
         EditText area=findViewById(R.id.area);
         EditText farm_location=findViewById(R.id.farm_location);
         EditText password=findViewById(R.id.password);
