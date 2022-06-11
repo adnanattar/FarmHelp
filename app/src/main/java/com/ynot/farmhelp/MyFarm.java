6276bc1cb5b9799d67ba8022505a4ab21f30303f
Priti <huparep2020@gmail.com>
2022-06-11 13:01:01 +0530

    Preeti Added findviewbyid

--- app/src/main/java/com/ynot/farmhelp/MyFarm.java
+++ app/src/main/java/com/ynot/farmhelp/MyFarm.java
@@ -3,6 +3,9 @@ package com.ynot.farmhelp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -20,5 +23,22 @@ public class MyFarm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_farm);
+
+        EditText fname = findViewById(R.id.fullName);
+        EditText email= findViewById(R.id.email);
+        EditText phone = findViewById(R.id.phone);
+
+        Spinner select_crop= findViewById(R.id.select_crop) ;
+        Spinner select_city= findViewById(R.id.select_city) ;
+        Spinner select_market= findViewById(R.id.select_market);
+        Spinner  select_soil = findViewById(R.id.select_soil);
+
+        EditText area=findViewById(R.id.area);
+        EditText farm_location=findViewById(R.id.farm_location);
+        EditText password=findViewById(R.id.password);
+
+        Button submit=findViewById(R.id.registerBtn);
+
+
     }
 }
--- app/src/main/java/com/ynot/farmhelp/Stock.java
+++ app/src/main/java/com/ynot/farmhelp/Stock.java
@@ -3,6 +3,7 @@ package com.ynot.farmhelp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class Stock extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Stock extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock);
+
+       Spinner select_crop =findViewById(R.id.select_city);
+       Spinner updated_data=findViewById(R.id.updated_data);
+
     }
 }
